4235ace189bf1d1350a1ee6d23c2d467e1613b8c
webdevaminul <webdev.aminul@gmail.com>
2024-08-17 16:53:39 +0600

    Added Sidebar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,43 @@ import useProducts from "../hooks/useProducts";
 
 export default function Home() {
   const [products] = useProducts();
-
   console.log(products);
+
   return (
-    <div className="container mx-auto">
-      <h1>Products</h1>
+    <div className="container mx-auto flex flex-col md:flex-row mt-4">
+      {/* Sidebar */}
+      <aside className="w-full md:w-1/4 bg-slate-100 p-4">
+        <h3 className="font-bold mb-2">Filter By Category</h3>
+
+        <ul>
+          <li>
+            <label>
+              <input type="radio" name="category" className="mr-2" />
+              All Categories
+            </label>
+          </li>
+          <li>
+            <label>
+              <input type="radio" name="category" className="mr-2" />
+              Electronics
+            </label>
+          </li>
+          <li>
+            <label>
+              <input type="radio" name="category" className="mr-2" />
+              Fashion
+            </label>
+          </li>
+          <li>
+            <label>
+              <input type="radio" name="category" className="mr-2" />
+              Home Appliances
+            </label>
+          </li>
+        </ul>
+      </aside>
+
+      <main>main</main>
     </div>
   );
 }
