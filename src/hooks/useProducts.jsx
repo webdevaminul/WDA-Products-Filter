b13a6c40ed2d7842b997dc50bf0d867211710063
webdevaminul <webdev.aminul@gmail.com>
2024-08-17 16:13:05 +0600

    Get the products array in client side

--- src/hooks/useProducts.jsx
+++ src/hooks/useProducts.jsx
@@ -1,10 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 
 export default function useProducts() {
-  const { data } = useQuery({
+  const {
+    data: products = [],
+    isLoading,
+    refetch,
+  } = useQuery({
     queryKey: ["products"],
     queryFn: async () => {
-      const res = await fetch("/products");
+      const res = await fetch("http://localhost:5000/products");
+      return res.json();
     },
   });
+
+  return [products, isLoading, refetch];
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,12 @@
+import useProducts from "../hooks/useProducts";
+
 export default function Home() {
-  return <div className="container mx-auto">Home</div>;
+  const [products] = useProducts();
+
+  console.log(products);
+  return (
+    <div className="container mx-auto">
+      <h1>Products</h1>
+    </div>
+  );
 }
